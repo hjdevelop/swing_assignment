b9a0f10cb6108ee9f41272e01912c42dd8eb7a73
hjdevelop <hyunjun6133@naver.com>
2024-04-13 21:32:02 +0900

    [Feat] : Main Activity BottomNav 세팅 및 Fragment 세팅

---
+++ app/src/main/java/com/example/swing_assignment/BookmarkFragment.kt
@@ -0,0 +1,25 @@
+package com.example.swing_assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class BookmarkFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.bookmark_fragment, container, false)
+    }
+}
--- app/src/main/java/com/example/swing_assignment/MainActivity.kt
+++ app/src/main/java/com/example/swing_assignment/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.swing_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.swing_assignment.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBottomNav()
+
+        // 초기 프래그먼트 설정
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, FeedFragment())
+                .commit()
+        }
+    }
+
+    //BottomNavigation 세팅
+    private fun setBottomNav() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_feed -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, FeedFragment()).commit()
+                    true
+                }
+                R.id.fragment_bookmark -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, BookmarkFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
