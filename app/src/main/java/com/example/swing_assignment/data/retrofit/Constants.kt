030e1e2e0cc8ccbf40fb19a9156852fdbf2e91cc
hjdevelop <hyunjun6133@naver.com>
2024-04-16 06:27:09 +0900

    [Feat] : baseUrl, key 추가

---
+++ app/src/main/java/com/example/swing_assignment/data/retrofit/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.swing_assignment.data.retrofit
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+        const val API_KEY = "CTf3ckEd9Q6ECPylTxObKfGKLaL8lg40gQCYKP3MIz0"
+    }
+}
